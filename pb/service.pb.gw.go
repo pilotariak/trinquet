da318291ece086977e622d3b075ff6844601d108
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-12-11 14:44:09 +0100

    FIX Uri for league
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- pb/service.pb.gw.go
+++ pb/service.pb.gw.go
@@ -206,7 +206,7 @@ var (
 
 	pattern_LeagueService_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "leagues"}, ""))
 
-	pattern_LeagueService_Get_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "leages", "name"}, ""))
+	pattern_LeagueService_Get_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "leagues", "name"}, ""))
 )
 
 var (
