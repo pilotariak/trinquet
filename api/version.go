415b41af6e1ff222c1080d576a017397f7212dbf
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-05-16 17:27:08 +0200

    update copyright
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- api/version.go
+++ api/version.go
@@ -1,10 +1,16 @@
-// Copyright (c) 2017 Orange Applications for Business.
+// Copyright (C) 2016, 2017 Nicolas Lamirault <nicolas.lamirault@gmail.com>
 
-// This software is confidential and proprietary information of
-// Orange Applications for Business. You shall not disclose such Confidential
-// Information and shall use it only in accordance with the terms of the
-// agreement you entecolors.red into. Unauthorized copying of this file, via any
-// medium is strictly prohibited.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package api
 
